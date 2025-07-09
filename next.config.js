c600e791a410b9c6270d63433363c211c0efe06d
lifan wang <lifanwang@MacBook-Pro-8.attlocal.net>
2025-07-09 02:27:03 -0700

    fix: remove invalid experimental.serverActions from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   images: {
     domains: [],
   },
@@ -11,4 +8,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig
